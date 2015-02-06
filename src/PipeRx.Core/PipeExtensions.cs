335eb29ed479b921c8346925992998bfe007fd96
jonathanstarnes <jonathanstarnes@gmail.com>
2015-02-06 10:00:19 -0600

    Initial Commit

---
+++ src/PipeRx.Core/PipeExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+
+namespace PipeRx.Core
+{
+    public static class PipeExtensions
+    {
+        public static IObservable<TOut> Pipe<TIn, TOut>(this IObservable<TIn> inlet, ISubject<TIn, TOut> segment)
+        {
+            inlet.Subscribe(segment);
+            return segment;
+        }
+
+        public static IObservable<TOut> Pipe<TIn, TOut>(this IObservable<TIn> inlet, Action<IObserver<TOut>, TIn> segment)
+        {
+            var replay = new ReplaySubject<TOut>();
+            inlet.Subscribe((s) => segment(replay, s));
+            return replay;
+        }
+
+        public static IObservable<T> Filter<T>(this IObservable<T> inlet, Predicate<T> filter)
+        {
+            var outlet = new ReplaySubject<T>();
+
+            inlet.Subscribe((value) =>
+            {
+                if (!filter(value))
+                {
+                    outlet.OnNext(value);
+                }
+            });
+
+            return outlet;
+        }
+
+        public static IObservable<T> Filter<T>(this IObservable<T> inlet, IEnumerable<Predicate<T>> filters)
+        {
+            var outlet = new ReplaySubject<T>();
+
+            inlet.Subscribe((value) =>
+            {
+                if (!filters.Any(filter => filter(value)))
+                {
+                    outlet.OnNext(value);
+                }
+            });
+
+            return outlet;
+        }
+    }
+}
---
+++ src/PipeRx.Core/PipelineSegment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace PipeRx.Core
+{
+    public abstract class PipelineSegment<TIn, TOut> : ISubject<TIn, TOut>
+    {
+        readonly ReplaySubject<TOut> _outlet = new ReplaySubject<TOut>();
+
+        protected abstract IEnumerable<TOut> ProcessInput(TIn value);
+
+        public void OnNext(TIn value)
+        {
+            Parallel.ForEach(ProcessInput(value), output => _outlet.OnNext(output));
+        }
+
+        public void OnError(Exception error)
+        {
+            _outlet.OnError(error);
+        }
+
+        public void OnCompleted()
+        {
+            _outlet.OnCompleted();
+        }
+
+        public IDisposable Subscribe(IObserver<TOut> observer)
+        {
+            return _outlet.Subscribe(observer);
+        }
+    }
+}
