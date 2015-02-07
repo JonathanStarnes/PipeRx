c88a31ae40fd2a7c62eebd703133cdbb2a767b52
JonathanStarnes <jonathanstarnes@gmail.com>
2015-02-06 20:45:35 -0600

    Added Missing Tests

---
+++ test/PipeRx.Test/FilterTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using PipeRx.Core;
+using Xunit;
+using Xunit.Should;
+
+namespace PipeRx.Test
+{
+    public class FilterTest
+    {
+        [Fact]
+        public void test_item_should_be_filtered_out()
+        {
+            var inlet = new ReplaySubject<string>();
+            var count = 0;
+            inlet
+                .Filter(s => s.Contains("filter"))
+                .Do(s=> count++)
+                .Subscribe(s => s.ShouldNotContain("filter"));
+
+            inlet.OnNext("in output");
+            inlet.OnNext("will be filtered out");
+
+            count.ShouldBe(1);
+        }
+
+        [Fact]
+        public void test_items_should_be_filtered_out()
+        {
+            var filters = new List<Predicate<string>>
+            {
+                s=> s.Contains("filter"), // Filter out strings with filter in them
+                s=> s.Length < 6 // filter out strings with a length less than 6
+            };
+
+            var inlet = new ReplaySubject<string>();
+            var count = 0;
+            inlet
+                .Filter(filters)
+                .Do(s => count++)
+                .Subscribe(s => s.ShouldBe("in output"));
+
+            inlet.OnNext("in output"); // only this item should be in the output
+            inlet.OnNext("will be filtered out"); 
+            inlet.OnNext("small");
+
+            count.ShouldBe(1);
+        }
+    }
+}
---
+++ test/PipeRx.Test/PipeTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reactive.Subjects;
+using PipeRx.Core;
+using Xunit;
+using Xunit.Should;
+
+namespace PipeRx.Test
+{
+    public class PipeTest
+    {
+        private const string ExpectedValue = "test";
+
+        [Fact]
+        public void input_of_pipe_should_propagate_through_the_action_to_the_output()
+        {
+            Action<IObserver<string>, string> testSegment = (observer, value) => observer.OnNext(value);
+            
+            var inlet = new ReplaySubject<string>();
+
+            inlet
+                .Pipe(testSegment)
+                .Subscribe(s => s.ShouldBe(ExpectedValue));
+
+            inlet.OnNext(ExpectedValue);
+            inlet.OnCompleted();
+        }
+
+        [Fact]
+        public void input_of_pipe_should_propagate_through_the_segment_to_the_output()
+        {
+            var output = string.Empty;
+
+            var inlet = new ReplaySubject<string>();
+
+            inlet
+                .Pipe(new TestSegment())
+                .Subscribe(s =>
+                {
+                    output = s;
+                });
+
+            inlet.OnNext(ExpectedValue);
+            inlet.OnCompleted();
+
+            output.ShouldBe(ExpectedValue);
+        }
+    }
+}
